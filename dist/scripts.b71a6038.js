e2157536e7f79c62dfc0b1c26cf63ce6362dc88f
August Aldred <AugustAldred@bpp.com>
2020-09-22 15:39:48 +0100

    updated build command

--- dist/scripts.b71a6038.js
+++ dist/scripts.b71a6038.js
@@ -249,7 +249,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55016" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55340" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
