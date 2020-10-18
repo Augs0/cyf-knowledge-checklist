977f3b4387acaef0b8ffb8b9fd97a99ac2f6be9f
Angel <mario.velez@sofka.com.co>
2020-10-18 13:39:53 -0500

    added button to save or download as pdf file

--- scripts.js
+++ scripts.js
@@ -1,31 +1,34 @@
-import './sass/styles.scss'
+import './sass/styles.scss';
 
 const greenLights = document.querySelectorAll('.green-light');
-greenLights.forEach(greenLight => {
-    greenLight.addEventListener('click', () => {
-        let wrapper = greenLight.closest('article');
-        let statement = wrapper.querySelector('p');
-        statement.setAttribute("class", "");
-        statement.classList.add('green');
-    });
+greenLights.forEach((greenLight) => {
+  greenLight.addEventListener('click', () => {
+    let wrapper = greenLight.closest('article');
+    let statement = wrapper.querySelector('p');
+    statement.setAttribute('class', '');
+    statement.classList.add('green');
+  });
 });
 
 const yellowLights = document.querySelectorAll('.yellow-light');
-yellowLights.forEach(yellowLight => {
-    yellowLight.addEventListener('click', () => {
-        let wrapper = yellowLight.closest('article');
-        let statement = wrapper.querySelector('p');
-        statement.setAttribute("class", "");
-        statement.classList.add('yellow');
-    });
+yellowLights.forEach((yellowLight) => {
+  yellowLight.addEventListener('click', () => {
+    let wrapper = yellowLight.closest('article');
+    let statement = wrapper.querySelector('p');
+    statement.setAttribute('class', '');
+    statement.classList.add('yellow');
+  });
 });
 
 const redLights = document.querySelectorAll('.red-light');
-redLights.forEach(redLight => {
-    redLight.addEventListener('click', () => {
-        let wrapper = redLight.closest('article');
-        let statement = wrapper.querySelector('p');
-        statement.setAttribute("class", "");
-        statement.classList.add('red');
-    });
+redLights.forEach((redLight) => {
+  redLight.addEventListener('click', () => {
+    let wrapper = redLight.closest('article');
+    let statement = wrapper.querySelector('p');
+    statement.setAttribute('class', '');
+    statement.classList.add('red');
+  });
 });
+
+const downloadButton = document.querySelector('.downloadPDF');
+downloadButton.addEventListener('click', () => window.print());
